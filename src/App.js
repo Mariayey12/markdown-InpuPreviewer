b0be099a06032f51c80d2b6b682cd045f0e60571
Maria Martinez <mmariayennifer.25@gmail>
2022-06-04 15:49:31 -0500

    git button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
 import Badge from "react-bootstrap/Badge";
-let marked = require("marked");
+import { marked } from 'marked'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: `
+     `,
+    };
+  }
+
+  updateMarkdown(markdown) {
+    this.setState({ markdown });
+  }
+
   render() {
     var inputStyle = {
       width: "400px",
@@ -34,6 +46,38 @@ export default class App extends React.Component {
             </div>
           </div>
           <div className="row mt-4">
+          <div className="col text-center" style={{ padding: "10px" }}>
+              <a
+                class="github-button"
+                href="https://github.com/Mariayey12"
+                data-size="large"
+                aria-label="Follow @Mariayey12 on GitHub"
+              >
+                Follow @lelouchB
+              </a>
+              <div style={{ marginLeft: "10px" }}></div>
+              <a
+                class="github-button"
+                href=""
+                data-icon="octicon-star"
+                data-size="large"
+                aria-label="Star lelouchB/markdown-previewer on GitHub"
+              >
+                Star
+              </a>
+
+              <a
+                class="github-button"
+                href="https://github.com/lelouchB/markdown-previewer/fork"
+                data-icon="octicon-repo-forked"
+                data-size="large"
+                aria-label="Fork lelouchB/markdown-previewer on GitHub"
+              >
+                Fork
+              </a>
+              </div>
+          </div>
+          <div className="row mt-4">
             <div className="col-md-6">
               <div className="col text-center">
                 <h4>
@@ -44,8 +88,14 @@ export default class App extends React.Component {
               </div>
 
               <div className="mark-input" style={inputStyle}>
-                <textarea className="input" style={inputStyle}>
-                  
+                <textarea
+                  className="input"
+                  style={inputStyle}
+                  value={this.state.markdown}
+                  onChange={(e) => {
+                    this.updateMarkdown(e.target.value);
+                  }}
+                >{console.log(this.state.markdown)}
                   {" "}
                 </textarea>
               </div>
@@ -58,7 +108,7 @@ export default class App extends React.Component {
                   </Badge>
                 </h4>
               </div>
-              <div style={outputStyle} ></div>
+              <div style={outputStyle}  dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}></div>
             </div>
           </div>
         </div>
